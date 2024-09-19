565d8c326ba14aaa5f9807552215a9b2bcc8fb6e
Nikhil Muddamsetty <Nikhil Muddamsetty>
2024-09-19 19:39:33 +0530

    Adds cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
